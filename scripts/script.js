bfa450b360f94e8b5eb21fcb9f30e740fb87ba06
Mohammad Abdelkhalek <138255130+mohamad-abdelkhalek@users.noreply.github.com>
2024-11-28 17:16:23 +0200

    Added toggler event listener on clicking <CSS> <JS>

--- scripts/script.js
+++ scripts/script.js
@@ -4,7 +4,7 @@ const sendMessageButton = document.querySelector("#send-message");
 const fileInput = document.querySelector("#file-input");
 const fileUploadWrapper = document.querySelector(".file-upload-wrapper");
 const fileCancelButton = document.querySelector("#file-cancel");
-
+const chatbotToggler = document.querySelector("#chatbot-toggler");
 
 // API setup
 const API_KEY = "AIzaSyCNs5D0Zh28dc8R2oW_v9OrmHoqq1XckD8";
@@ -175,3 +175,6 @@ document.querySelector(".chat-form").appendChild(picker);
 sendMessageButton.addEventListener("click", (e) => handleOutgoingMessage(e));
 
 document.querySelector("#file-upload").addEventListener("click", () => fileInput.click());
+
+chatbotToggler.addEventListener("click", () => document.body.classList.toggle("show-chatbot"));
+
